eec36b0b06eabd8bdca2218d4bd0c28c96b6e32c
SA2C WWW <sa2c-support@swansea.ac.uk>
2018-02-08 13:23:52 +0000

    detect python version, create venv if necessary

--- make.sh
+++ make.sh
@@ -1,2 +1,22 @@
+if [ -x "$(command -v python3)" ]
+then
+    PYTHON=python3
+elif [ -x "$(command -v python36)" ]
+then
+    PYTHON=python36
+else
+    echo Python 3 not found
+    exit 1
+fi
+
+if [ ! -f "clinic/bin/activate" ]
+then
+    $PYTHON -m venv seminar
+    source seminar/bin/activate
+    pip install -r requirements.txt
+else
+    source seminar/bin/activate
+fi
+
 source seminar/bin/activate
 python3 ./generate.py seminars site/index.html --annual_template=annual_template.html
